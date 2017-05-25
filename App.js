909afe227bd975b7ece71dd9b93c8612aed6a1e2
gustavoleite <gustavo.jog@hotmail.com>
2017-05-25 18:19:50 -0300

    Test common components

--- App.js
+++ App.js
@@ -1,23 +1,57 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Card, CardSection } from './src/components/common';
 
 export default class App extends React.Component {
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Olá!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the devdadseloper menu.</Text>
+      <View>
+        <Text style={styles.viewStyle}>
+            Meus documentos
+          </Text>
+          <Card>
+            <CardSection>
+              <View style={styles.headerContentStyle}>
+                <Text style={styles.headerTextStyle}>Nome do documento</Text>
+                <Text>Número do documento</Text>
+              </View>
+            </CardSection>
+            <CardSection>
+              <Image
+                style={styles.imageStyle}
+              />
+            </CardSection>
+          </Card>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
     justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    textAlign: 'center'
   },
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+
+  headerTextStyle: {
+    fontSize: 18
+  },
+
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
 });
