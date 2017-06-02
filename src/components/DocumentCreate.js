9768cc8cfdb12a3916316944736b8f22e753d37e
gustavoleite <gustavo.jog@hotmail.com>
2017-06-02 19:02:11 -0300

    Initial config Google Analytics

--- src/components/DocumentCreate.js
+++ src/components/DocumentCreate.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import {
+  GoogleAnalyticsTracker,
+  GoogleTagManager,
+  GoogleAnalyticsSettings
+} from 'react-native-google-analytics-bridge';
 import { documentUpdate, documentClear, documentCreate } from '../actions';
 import { Card, CardSection, Button } from './common';
 import DocumentForm from './DocumentForm';
 
 class DocumentCreate extends Component {
   componentWillMount() {
+    const tracker = new GoogleAnalyticsTracker('UA-100420550-1');
+    tracker.trackScreenView('Home');
+    tracker.trackEvent('Customer', 'New');
+
+    GoogleAnalyticsSettings.setDispatchInterval(10);
+    GoogleAnalyticsSettings.setDryRun(false);
     this.props.documentClear();
   }
 
